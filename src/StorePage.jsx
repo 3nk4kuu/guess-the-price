92b3145a914c070df3af33c122451e30d0643836
Keziah Muyna <kmuyna@Keziahs-MacBook-Pro.local>
2026-07-05 00:56:05 -0700

    Add difficulty selector and have difficulty change from the api url

--- src/StorePage.jsx
+++ src/StorePage.jsx
@@ -5,30 +5,52 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 
 function StorePage() {
   const [game, setGame] = useState(null);
   const [gameList, setGameList] = useState([]);
+  const [difficulty, setDifficulty] = useState("easy");
 
   const [guess, setGuess] = useState("");
   const [message, setMessage] = useState("");
   const [isCorrect, setIsCorrect] = useState(false);
+  const [score, setScore] = useState(0);
 
   const getRandomGame = () => {
     if (game && !isCorrect) {
       setMessage("Must guess the price of current game before moving on.");
       return;
     }
-    const randomPage = Math.floor(Math.random() * 50);
 
     setGuess("");
     setMessage("");
     setIsCorrect(false);
 
     console.log("getting game data");
-    fetch(
-      `https://www.cheapshark.com/api/1.0/deals?storeID=1&pageNumber=${randomPage}`,
-    )
+
+    // set dfualts - medium
+    let minReviews = 5000;
+    let sortParams = ""; 
+    let maxPages = 20;
+
+    if (difficulty === "easy") {
+      minReviews = 50000;
+      sortParams = "&sortBy=ReviewCount&desc=1";
+      maxPages = 3;
+    } else if (difficulty === "hard") {
+      minReviews = 500;
+      sortParams = "&sortBy=ReviewCount&desc=0";
+      maxPages = 50;
+    }
+
+    const randomPage = Math.floor(Math.random() * maxPages);
+    // easy 50k+, medium 5k+, hard 500+
+    
+   fetch(`https://www.cheapshark.com/api/1.0/deals?storeID=1&pageNumber=${randomPage}&minimumReviewCount=${minReviews}${sortParams}`)
       .then((response) => {
         if (response.status === 429) {
           throw new Error("RATE_LIMIT");
@@ -38,18 +60,14 @@ function StorePage() {
       .then((data) => {
         setGameList(data);
 
-        const popularGames = data.filter(
-          (item) => parseInt(item.steamRatingCount) >= 5000,
-        );
-
-        if (popularGames.length > 0) {
-          const randomIndex = Math.floor(Math.random() * popularGames.length);
-          const selectedGame = popularGames[randomIndex];
+        if (data.length > 0) {
+          const randomIndex = Math.floor(Math.random() * data.length);
+          const selectedGame = data[randomIndex];
           console.log("got game data successfully", selectedGame);
-          
+
           setGame(selectedGame);
         } else {
-            console.log("No games with 5000+ reviews on page", randomPage);
+          console.log(`no games with ${minReviews}+ reviews on page`, randomPage);
           setTimeout(getRandomGame, 1000);
         }
       })
@@ -91,7 +109,7 @@ function StorePage() {
       setMessage("Too high! Try again.");
       setGuess("");
     }
-    // TODO: change to hot/cold colors to later
+    // TODO: change to hot/cold colors to later !!!
   };
 
   const handleKeyDown = (enter) => {
@@ -118,6 +136,21 @@ function StorePage() {
         Guess The Price
       </Typography>
 
+      <FormControl sx={{width: "25%", mx: "auto"}}>
+        <InputLabel id="game-difficulty">Difficulty</InputLabel>
+        <Select
+          labelId="game-difficulty"
+          id="difficulty-select"
+          value={difficulty}
+          label="Difficulty"
+          onChange={(diff) => setDifficulty(diff.target.value)}
+        >
+          <MenuItem value="easy">Easy</MenuItem>
+          <MenuItem value="medium">Medium</MenuItem>
+          <MenuItem value="hard">Hard</MenuItem>
+        </Select>
+      </FormControl>
+
       <Button
         sx={{
           width: "25%",
@@ -134,7 +167,7 @@ function StorePage() {
       {/* GAME CARD */}
       {/* TODO: make game card into component and put in carousel of 10 - inspo discovery queue */}
       {game && (
-        <Card sx={{ minWidth: 275, maxWidth: 500, mx: "auto" }}>
+        <Card sx={{ minWidth: 275, maxWidth: "50%", mx: "auto" }}>
           <CardContent>
             <Typography variant="h5" component="div" gutterBottom>
               {game.title}
